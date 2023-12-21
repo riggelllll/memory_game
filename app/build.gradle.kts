2fd38bc5f427a0a38d86ad1992033adaaeef73ad
Vladyslav Koniukhov <riggelllll@gmail.com>
2023-12-21 14:40:13 +0200

    Added Navigation component

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ kapt {
 }
 
 dependencies {
+    val nav_version = "2.7.6"
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -49,8 +50,12 @@ dependencies {
     implementation("androidx.core:core-splashscreen:1.1.0-alpha02")
     implementation("com.google.dagger:hilt-android:2.50")
     kapt("com.google.dagger:hilt-android-compiler:2.50")
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
 
     testImplementation("junit:junit:4.13.2")
+
+    androidTestImplementation("androidx.navigation:navigation-testing:$nav_version")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
